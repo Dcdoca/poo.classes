1ffe56813c35b9e5581e585e0758500577b3f425
Dcdoca <antoniomarcorrs@gmail.com>
2024-06-27 21:57:00 -0300

    exercicio de poo

---
+++ src/main/java/com/poo/classes/Pessoa.java
@@ -0,0 +1,20 @@
+package com.poo.classes;
+
+public class Pessoa {
+String rg;
+String cpf;
+String nome;
+String endereco;
+
+
+
+
+//metdos de exibir informaçôes da pessoa
+public void exibirInformacoes(){
+    System.out.println("rg: " + rg);
+    System.out.println("cpf: " + cpf);
+    System.out.println("nome: " + nome);
+    System.out.println("endereco: " + endereco);
+}
+
+}
