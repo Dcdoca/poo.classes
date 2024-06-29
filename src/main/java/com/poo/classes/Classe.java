d10591c2ff199ade5c80d2c2f9111c18749a1f1b
Dcdoca <antoniomarcorrs@gmail.com>
2024-06-28 21:25:59 -0300

    atulizacao da sessao pessoa

--- src/main/java/com/poo/classes/Classe.java
+++ src/main/java/com/poo/classes/Classe.java
@@ -3,3 +3,15 @@ package com.poo.classes;
 public class Classe {
 
 }
+String cpfNumerico = CPF.replaceAll("\D", "");
+        if ((cpfNumerico.length())==11) {
+
+            this.CPF=CPF;
+        }
+public void setRG (String RG) {
+        String rgNumerico = RG.replaceAll("\D", "");
+        if ((rgNumerico.length())==9) {
+
+            this.RG=RG;
+        }
+    }
--- src/main/java/com/poo/classes/Pessoa.java
+++ src/main/java/com/poo/classes/Pessoa.java
@@ -1,20 +1,45 @@
 package com.poo.classes;
 
 public class Pessoa {
-String rg;
-String cpf;
-String nome;
-String endereco;
+private String rg;
+private String cpf;
+private String nome;
+private String endereco;
 
 
 
+//metodos getters e setters
+public String getNome(){
+    return nome;
+}
+public void setNome(String nome){
+    this.nome = nome;
+}
+public String getRg(){
+    return rg;
+}
+public void setRg(String rg){
+    this.rg = rg;
+}
+public String getCpf(){
+    return cpf;
+}
+public void setCpf(String cpf){
+    this.cpf = cpf;
+}
+public String getEndereco(){
+    return endereco;
+}
+public void setEndereco(String endereco){
+    this.endereco = endereco;
+}
 
 //metdos de exibir informaçôes da pessoa
 public void exibirInformacoes(){
-    System.out.println("rg: " + rg);
-    System.out.println("cpf: " + cpf);
-    System.out.println("nome: " + nome);
-    System.out.println("endereco: " + endereco);
+    System.out.println("rg: " + getRg());
+    System.out.println("cpf: " + getCpf());
+    System.out.println("nome: " + getNome());
+    System.out.println("endereco: " + getEndereco());
 }
 
 }
