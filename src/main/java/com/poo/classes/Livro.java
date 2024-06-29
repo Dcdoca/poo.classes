b6c287551b4c8cbbeaa80d90a23b64db105e1005
Dcdoca <antoniomarcorrs@gmail.com>
2024-06-28 21:39:46 -0300

    atulizacao da sessao livro

--- src/main/java/com/poo/classes/Livro.java
+++ src/main/java/com/poo/classes/Livro.java
@@ -1,7 +1,37 @@
 package com.poo.classes;
 
 public class Livro {
-String edicao;
-String titulo;
-int ano;
+private String edicao;
+private String titulo;
+private int ano;
+
+//metodos getters e setters
+
+public String getEdicao(){
+    return edicao;
+}
+public void setEdicao(String edicao){
+    this.edicao = edicao;
+}
+public String getTitulo(){
+    return titulo;
+}
+public void setTitulo(String titulo){
+    this.titulo = titulo;
+}
+public int getAno(){
+    return ano;
+}
+public void setAno(int ano){
+    this.ano = ano;
+}
+
+//metdos de exibir informaçôes do livro
+public void exibirInformacoes(){
+    System.out.println("rg: " + getEdicao());
+    System.out.println("cpf: " + getTitulo());
+    System.out.println("nome: " + getAno());
+}
+
+
 }
